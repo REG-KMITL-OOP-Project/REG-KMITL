8fc225c768bb5a5e25dfc743a95db0bcee1e2c32
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2025-03-01 10:29:25 +0700

    Add Account contructor and basic getter setter method for all attribute kubb

--- src/main/java/dev/it22/kmitl/reg/model/auth/Account.java
+++ src/main/java/dev/it22/kmitl/reg/model/auth/Account.java
@@ -14,4 +14,84 @@ public abstract class Account {
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
+
+    public Account(String id, UserRole role, String email, String username, UserPrefix prefix, String firstName, String lastName, Date dateOfBirth) {
+        this.id = id;
+        this.role = role;
+        this.email = email;
+        this.username = username;
+        this.prefix = prefix;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserPrefix getPrefix() {
+        return prefix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPrefix(UserPrefix prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFullName() {
+        return prefix + " " + firstName + " " + lastName;
+    }
+
 }
