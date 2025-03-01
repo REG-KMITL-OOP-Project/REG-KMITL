fb4d07dc8afdd84bb498c84318238102776c2109
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2025-03-01 10:34:20 +0700

    Add Admin basic constructor

--- src/main/java/dev/it22/kmitl/reg/model/auth/Admin.java
+++ src/main/java/dev/it22/kmitl/reg/model/auth/Admin.java
@@ -1,5 +1,17 @@
 package dev.it22.kmitl.reg.model.auth;
 
-public class Admin extends Account {
+import dev.it22.kmitl.reg.constant.UserPrefix;
+import dev.it22.kmitl.reg.constant.UserRole;
 
+import java.util.Date;
+
+public class Admin extends Account {
+    public Admin(String id, String email, String username, UserPrefix prefix, String firstName, String lastName, Date dateOfBirth) {
+        super(id, UserRole.ADMIN, email, username, prefix, firstName, lastName, dateOfBirth);
+    }
+
+    public void approveRequest() {
+        System.out.println("Request approved");
+        System.out.println("I will back to this and implement the logic later jup jup");
+    }
 }
