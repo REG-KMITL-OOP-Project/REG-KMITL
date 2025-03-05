27a78d909905bcbfddc0a1fa9c3f38550d281966
Hundredz <151929262+NnWAlovemeow@users.noreply.github.com>
2025-03-05 13:26:28 +0700

    Create RoundedTextField.java

---
+++ src/main/java/dev/it22/kmitl/reg/utils/RoundedTextField.java
@@ -0,0 +1,46 @@
+package dev.it22.kmitl.reg.utils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedTextField extends JTextField {
+    private int radius;
+
+    public RoundedTextField(int radius) {
+        super();
+        this.radius = radius;
+        setOpaque(false); // Ensure transparency
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Draw rounded background
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+
+        g2.dispose();
+
+        super.paintComponent(g);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(getForeground());
+        g2.drawRoundRect(0, 0, 0, 0, radius, radius);
+
+        g2.dispose();
+    }
+
+    @Override
+    public boolean contains(int x, int y) {
+        return new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), radius, radius).contains(x, y);
+    }
+}
