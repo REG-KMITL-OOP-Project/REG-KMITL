9e52f7c453718755d8791af30443504929da4c83
Hundredz <151929262+NnWAlovemeow@users.noreply.github.com>
2025-03-06 13:47:47 +0700

    Update EditEventPage.java
    add FocusListener and text place holder logic

--- src/main/java/dev/it22/kmitl/reg/ui/event/EditEventPage.java
+++ src/main/java/dev/it22/kmitl/reg/ui/event/EditEventPage.java
@@ -5,9 +5,11 @@ import dev.it22.kmitl.reg.utils.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.*;
 
-public class EditEventPage implements ActionListener {
+public class EditEventPage implements ActionListener , FocusListener {
     private  JFrame frame;
     private JPanel panelBig,panelRek1,panelRek2,panelRek3,panelRek4,panelRek5,panelEdit,panelSave,panelDel;
     private RoundedButton save,delete;
@@ -16,6 +18,8 @@ public class EditEventPage implements ActionListener {
     private RoundedTextArea description;
     private JComboBox eventType;
     private Font innerFont, regularFont;
+    private boolean showEventName , showDateStart, 
+    showDateEnd, showDescription;
 
     public EditEventPage(JFrame frame){
         this.frame = frame;
@@ -62,27 +66,39 @@ public class EditEventPage implements ActionListener {
         panelBig.add(Box.createVerticalStrut(10));
 
         panelBig.add(panelRek1);
+            showEventName = true;
             eventName.setText("   EVENT NAME");
             eventName.setFont(innerFont);
             eventName.setForeground(Color.GRAY);
             eventName.setPreferredSize(new Dimension((int)(frame.getWidth() / 1.2),(frame.getHeight() / 4) - 120));
+
+            eventName.addFocusListener(this);
+
+
         panelRek1.add(eventName);
 
         panelBig.add(panelRek2);
+            showDescription = true;
             description.setText("   DESCRIPTION");
             description.setPreferredSize(new Dimension((int)(frame.getWidth() / 1.2),(frame.getHeight() / 4)));
             description.setFont(innerFont);
             description.setForeground(Color.GRAY);
+
+            description.addFocusListener(this);
         panelRek2.add(description);
 
         panelBig.add(panelRek3);
         panelRek3.setLayout( new FlowLayout(FlowLayout.CENTER, 190, 0));
         panelRek3.add(dateStart);
             dateStart.setHorizontalAlignment(SwingConstants.CENTER);
+            showDateStart = true;
             dateStart.setText("DD/MM/YY");
             dateStart.setPreferredSize(new Dimension((int)((frame.getWidth()-500)/3.4),(frame.getHeight() / 4) - 120));
             dateStart.setFont(innerFont);
             dateStart.setForeground(Color.GRAY);
+
+            dateStart.addFocusListener(this);
+
         panelRek3.add(to);
             to.setOpaque(true);
             to.setBackground(Config.primaryColor_hard);
@@ -92,10 +108,13 @@ public class EditEventPage implements ActionListener {
             to.setHorizontalAlignment(SwingConstants.CENTER);
         panelRek3.add(dateEnd);
             dateEnd.setHorizontalAlignment(SwingConstants.CENTER);
+            showDateEnd = true;
             dateEnd.setText("DD/MM/YY");
             dateEnd.setPreferredSize(new Dimension((int)((frame.getWidth()-500)/3.4),(frame.getHeight() / 4) - 120));
             dateEnd.setFont(innerFont);
             dateEnd.setForeground(Color.GRAY);
+
+            dateEnd.addFocusListener(this);
 
         panelBig.add(panelRek4);
         eventType.addItem("   CHOOSE CATEGORY");
@@ -115,7 +134,7 @@ public class EditEventPage implements ActionListener {
         panelRek5.add(panelSave);
         panelSave.setLayout( new FlowLayout(FlowLayout.LEFT,93,0));
         save.setForeground(Color.BLACK);
-        save.setBackground(new Color(255,247,237));
+        save.setBackground(Config.primaryColor_base);
         save.setFont(Config.HEADER_SEMIBOLD[2]);
         save.setPreferredSize(new Dimension((int)((frame.getWidth()-500)/2.7),(frame.getHeight() / 4) - 120));
 
@@ -142,7 +161,7 @@ public class EditEventPage implements ActionListener {
         new EditEventPage(Config.createAndShowGUI());
     }
 
-    private JButton del , cal ;
+    private RoundedButton del , cal ;
     private JDialog dialog ;
 
     public void actionPerformed(ActionEvent ev){
@@ -153,10 +172,11 @@ public class EditEventPage implements ActionListener {
                 JPanel P = new JPanel();
                 JPanel panel = new JPanel();
                 JLabel header = new JLabel("ARE YOU SURE WANT TO DELETE");
-                del = new JButton("DELETE");
-                cal = new JButton("CANCEL");
+                del = new RoundedButton ("DELETE" , 22);
+                cal = new RoundedButton ("CANCEL" , 22);
 
                 header.setForeground(Config.primaryColor_hard);
+
                 header.setFont(Config.HEADER_SEMIBOLD[1]);
                 panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
                 panel.add(Box.createVerticalStrut(50));
@@ -174,6 +194,7 @@ public class EditEventPage implements ActionListener {
                 del.setForeground(Color.WHITE);
                 del.setBackground(Config.primaryColor_harder);
                 del.setFont(Config.HEADER_SEMIBOLD[2]);
+
                 del.setPreferredSize(new Dimension((int)((frame.getWidth()-500)/3.5),(frame.getHeight() / 4) - 120));
 
                 del.addActionListener(this);
@@ -199,5 +220,53 @@ public class EditEventPage implements ActionListener {
             dialog.setVisible(false);
         }
 
+    }
+
+    public void focusGained(FocusEvent fg){
+         if (fg.getSource().equals(eventName) && showEventName){
+            eventName.setText("");
+            eventName.setForeground(Color.BLACK);
+            showEventName = false;
+
+         }else if(fg.getSource().equals(description) && showDescription){
+             description.setText("");
+             description.setForeground(Color.BLACK);
+             showDescription = false;
+
+         }else if (fg.getSource().equals(dateStart) && showDateStart){
+             dateStart.setText("");
+             dateStart.setForeground(Color.BLACK);
+             showDateStart = false;
+
+         }else if (fg.getSource().equals(dateEnd) && showDateEnd) {
+             dateEnd.setText("");
+             dateEnd.setForeground(Color.BLACK);
+             showDateEnd = false;
+         }
+    }
+
+    public void focusLost(FocusEvent e){
+        if (eventName.getText().isEmpty()) {
+            showEventName = true;
+            eventName.setText("   EVENT NAME");
+            eventName.setForeground(Color.GRAY);
+        }
+
+        if(description.getText().isEmpty()){
+            showDescription = true;
+            description.setText("   DESCRIPTION");
+            description.setForeground(Color.GRAY);
+        }
+
+        if (dateStart.getText().isEmpty()) {
+                showDateStart = true;
+                dateStart.setText("DD/MM/YY");
+                dateStart.setForeground(Color.GRAY);
+
+        }if (dateEnd.getText().isEmpty()) {
+                showDateEnd = true;
+                dateEnd.setText("DD/MM/YY");
+                dateEnd.setForeground(Color.GRAY);
+        }
     }
 }
