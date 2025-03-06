5107b338cbd6cb2c0f2654cf2457ba078c0ae054
bbimxn <67070102@kmitl.ac.th>
2025-03-06 22:55:25 +0700

    set layout & change color

--- src/main/java/dev/it22/kmitl/reg/ui/event/ExamSchedulePage.java
+++ src/main/java/dev/it22/kmitl/reg/ui/event/ExamSchedulePage.java
@@ -19,26 +19,32 @@ public class ExamSchedulePage {
     private JButton home;
 
     //body-information
-    private JPanel allInfo, testFormat, allchosen, blank, choseYear,chosen, stdInfo;
+    private JPanel allInfo, testFormat, allchosen, choseYear,choseSem,choseExam, stdInfo;
     private JLabel ID, name, faculty, branch;
     private JComboBox year, semester, exam ;
 
     //body-schedule
-    private  JTable examSchedule;
+    private JTable examSchedule;
     private JScrollPane scrollPane;
-    private JPanel blank2,blank3;
 
     private String years[] = {"2568", "2567", "2566"};
-    private String semesters[] = {"1", "2"};
+    private String semesters[] = {"Semester 1", "Semester 2"};
     private String exams[] = {"Midterm","Final"};
     private String columnNames[] = {"DD/MM/YY","time", "subject-ID","subject","type","seat","room"};
-    private Object testData[][] = {{"DD/MM/YY","09.30-18.00","060111222","OOP","practical","A4","L123"}};
+    private Object testData[][] = {{"DD/MM/YY","09.30-18.00","060111222","OOP","practical","A4","L123"},
+            {"DD/MM/YY","09.30-18.00","060111222","OOP","practical","A4","L123"},
+            {"DD/MM/YY","09.30-18.00","060111222","OOP","practical","A4","L123"},
+            {"DD/MM/YY","09.30-18.00","060111222","OOP","practical","A4","L123"}};
 
 
     public ExamSchedulePage(JFrame frame){
         this.frame = frame;
         pn1 = new JPanel();
         pn2 = new JPanel();
+
+        pn1.setBackground(null);
+        pn2.setBackground(null);
+
 
         //head-menubar
         bar = new JMenuBar();
@@ -51,19 +57,38 @@ public class ExamSchedulePage {
         tarangsob = new JLabel("Exam Schedule");
         home = new JButton("Home");
 
+        tarangsob.setForeground(Config.primaryColor_base);
+        tarangsob.setFont(Config.HEADER_SEMIBOLD[1]);;
+
         //body-information
         allInfo = new JPanel();
         testFormat = new JPanel();
         allchosen = new JPanel();
-        blank = new JPanel();
+
         choseYear = new JPanel();
-        chosen = new JPanel();
+        choseSem= new JPanel();
+        choseExam = new JPanel();
         stdInfo = new JPanel();
+
+        allInfo.setBackground(null);
+        testFormat.setBackground(null);
+        allchosen.setBackground(null);
+        choseYear.setBackground(null);
+        choseSem.setBackground(null);
+        choseExam.setBackground(null);
+        stdInfo.setBackground(null);
+
 
         ID = new JLabel("Student ID : ");
         name = new JLabel("Name : ");
         faculty = new JLabel("Faculty : ");
         branch = new JLabel("Branch : ");
+
+        ID.setForeground(Color.WHITE);
+        name.setForeground(Color.WHITE);
+        faculty.setForeground(Color.WHITE);
+        branch.setForeground(Color.WHITE);
+
 
         year = new JComboBox(years);
         semester = new JComboBox(semesters);
@@ -72,6 +97,7 @@ public class ExamSchedulePage {
         //place components
         //head
         //head-menubar
+        bar.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
         bar.add(home);
         bar.add(ETC);
         bar.add(tarangsob);
@@ -82,22 +108,26 @@ public class ExamSchedulePage {
         ETC.add(share);
 
         //body-schedule
-        blank2 = new JPanel();
-        blank3 = new JPanel();
         DefaultTableModel model = new DefaultTableModel(testData, columnNames);
         examSchedule = new JTable(model);
         scrollPane = new JScrollPane(examSchedule);
+        scrollPane.setBackground(null);
+        scrollPane.getViewport().setBackground(null);
+
 
         //panel-year button
-        choseYear.setBackground(null);
+        choseYear.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
         choseYear.setLayout(new GridLayout(1, 1));
         choseYear.add(year);
 
-        //panel-semester and exam button
-        chosen.setBackground(null);
-        chosen.setLayout(new GridLayout(1, 2));
-        chosen.add(semester);
-        chosen.add(exam);
+        //panel-semester
+        choseSem.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+        choseSem.setLayout(new GridLayout(1, 2));
+        choseSem.add(semester);
+
+        //panel-exam button
+        choseExam.setLayout(new GridLayout(1, 1));
+        choseExam.add(exam);
 
         //panel-studentInfo
         stdInfo.setLayout(new GridLayout(2, 2));
@@ -106,26 +136,30 @@ public class ExamSchedulePage {
         stdInfo.add(faculty);
         stdInfo.add(branch);
 
+
         allInfo.setLayout(new BorderLayout());
-        testFormat.setLayout(new GridLayout(2, 1));
-        allchosen.setLayout(new BorderLayout());
+        testFormat.setLayout(new GridLayout(1, 2));
+        allchosen.setLayout(new GridLayout(2, 1));
 
         //layout-all semester year exam button
-        testFormat.add(choseYear);
-        testFormat.add(chosen);
-        allchosen.add(blank, BorderLayout.WEST);
-        allchosen.add(testFormat, BorderLayout.CENTER);
+        testFormat.add(choseSem);
+        testFormat.add(choseExam);
+        allchosen.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15));
+        allchosen.add(choseYear);
+        allchosen.add(testFormat);
 
+        stdInfo.setBackground(Config.bgColor_base.darker());
+        stdInfo.setBorder(BorderFactory.createEmptyBorder(0, 15, 0, 0));
+        allInfo.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
         allInfo.add(allchosen, BorderLayout.WEST);
         allInfo.add(stdInfo, BorderLayout.CENTER);
 
+
         pn1.setLayout(new BorderLayout());
-        //pn1.add(head, BorderLayout.NORTH);
         pn1.add(allInfo, BorderLayout.SOUTH);
 
         pn2.setLayout(new BorderLayout());
-        pn2.add(blank2, BorderLayout.WEST);
-        pn2.add(blank3, BorderLayout.EAST);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 30, 30, 30));
         pn2.add(scrollPane, BorderLayout.CENTER);
 
         frame.setLayout(new BorderLayout());
