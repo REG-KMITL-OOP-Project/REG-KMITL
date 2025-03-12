92cf11e73b8014d24e0179df3f9b7f17ca76c89d
Hundredz <151929262+NnWAlovemeow@users.noreply.github.com>
2025-03-12 13:46:12 +0700

    Add Add Subject Page

--- src/main/java/dev/it22/kmitl/reg/ui/Homepage_regis/AdminAddSubject.java
+++ src/main/java/dev/it22/kmitl/reg/ui/Homepage_regis/AdminAddSubject.java
@@ -1,6 +1,8 @@
 package dev.it22.kmitl.reg.ui.Homepage_regis;
 import dev.it22.kmitl.reg.ui.event.AdminAddEvent;
 import dev.it22.kmitl.reg.utils.Config;
+import dev.it22.kmitl.reg.utils.CustomCombobox;
+import dev.it22.kmitl.reg.utils.RoundedTextField;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,7 +11,7 @@ public class AdminAddSubject {
     private JFrame frame ;
     private JPanel panelBig,panelRek1,panelRek2,panelRek3,panelRek4,panelRek5,panelHead;
     private JLabel addSubject;
-    private  JTextField year,name,code,note,condition;
+    private RoundedTextField year,name,code,note,condition;
     private JComboBox type,kit,group,branch;
     private Font innerFont, regularFont;
 
@@ -18,16 +20,31 @@ public class AdminAddSubject {
         panelBig = new JPanel();
         panelHead = new JPanel();
         panelRek1 = new JPanel();
+        panelRek2 = new JPanel();
+        panelRek3 = new JPanel();
+        panelRek4 = new JPanel();
+        panelRek5 = new JPanel();
         addSubject = new JLabel("              Add Subject");
-        year = new JTextField(10);
+        year = new RoundedTextField(22);
+        name = new RoundedTextField(22);
+        code = new RoundedTextField(22);
+        note = new RoundedTextField(22);
+        condition = new RoundedTextField(22);
+        type = new JComboBox();
+        kit = new JComboBox();
+        group = new JComboBox();
+        branch = new JComboBox();
         regularFont = Config.NORMAL_REGULAR;
         innerFont = regularFont.deriveFont(15f);
 
         panelBig.setBackground(null);
         panelHead.setBackground(null);
         panelRek1.setBackground(null);
+        panelRek2.setBackground(null);
+        panelRek3.setBackground(null);
+        panelRek4.setBackground(null);
+        panelRek5.setBackground(null);
 
-        frame.add(panelHead);
         panelHead.setLayout( new BorderLayout());
         panelHead.add(panelBig);
         panelBig.setLayout(new BoxLayout(panelBig, BoxLayout.Y_AXIS));
@@ -44,8 +61,59 @@ public class AdminAddSubject {
         year.setText("   ภาคการศึกษา");
         year.setFont(innerFont);
         year.setForeground(Color.GRAY);
-        year.setPreferredSize(new Dimension((int)(frame.getWidth() / 1.2),(frame.getHeight() / 4) - 120));
+        year.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+
+        panelRek1.add(type);
+        type.addItem("ประเภท");
+        type.addItem("ทฤษฏี");
+        type.addItem("ปฏิบัติ");
+        type.setRenderer(new CustomCombobox());
+        type.setMaximumRowCount(3);
+        type.setFont(Config.NORMAL_REGULAR);
+        type.setFont(innerFont);
+        type.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+
+        panelRek1.add(kit);
+        kit.addItem("หน่วยกิต");
+        kit.addItem("1");
+        kit.addItem("2");
+        kit.addItem("3");
+        kit.setRenderer(new CustomCombobox());
+        kit.setMaximumRowCount(3);
+        kit.setFont(Config.NORMAL_REGULAR);
+        kit.setFont(innerFont);
+        kit.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+
         panelBig.add(panelRek1);
+
+        panelRek2.add(group);
+        group.addItem("คณะ");
+        group.addItem("เทคโนโลยีสารสนเทศ");
+        group.setRenderer(new CustomCombobox());
+        group.setMaximumRowCount(3);
+        group.setFont(Config.NORMAL_REGULAR);
+        group.setFont(innerFont);
+        group.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+
+        panelRek2.add(branch);
+        branch.addItem("สาขา");
+        branch.addItem("เทคโนโลยีสารสนเทศ");
+        branch.addItem("วิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ");
+        branch.addItem("เทคโนโลยีปัญญาประดิษฐ์");
+        branch.setRenderer(new CustomCombobox());
+        branch.setMaximumRowCount(3);
+        branch.setFont(Config.NORMAL_REGULAR);
+        branch.setFont(innerFont);
+        branch.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+
+        panelRek2.add(code);
+        code.setText("   รหัสวิชาค");
+        code.setFont(innerFont);
+        code.setForeground(Color.GRAY);
+        code.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+
+        panelBig.add(panelRek2);
+
         panelHead.add(panelBig);
         frame.add(panelHead);
 
