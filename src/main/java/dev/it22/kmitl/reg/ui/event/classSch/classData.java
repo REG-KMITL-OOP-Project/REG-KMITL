0133975e21dc1c9a2d1147eed7a52e23a8135677
Jarinya19y <67070019@kmitl.ac.th>
2025-03-28 00:59:43 +0700

    add try-catch for classData

--- src/main/java/dev/it22/kmitl/reg/ui/event/classSch/classData.java
+++ src/main/java/dev/it22/kmitl/reg/ui/event/classSch/classData.java
@@ -25,39 +25,44 @@ public class classData {
     }
 
 
-    public String[] getSubject(String day) throws SQLException {
-        String q = null;
-        if (user instanceof Student) {
-            q = "SELECT room, course_id, section_id FROM section WHERE section = " + ((Student)user).getSection() + " AND day_of_week = '" + day + "'" ;
-        }
-        else if (user instanceof Prof){
-            q = "SELECT room, course_id, section_id FROM section WHERE prof_id = " + ((Prof)user).getProf_id() + " AND day_of_week = '" + day + "'" ;
-        }
-        ResultSet rs = db.getQuery(q);
+    public String[] getSubject(String day){
         String[] subject = new String[12];
-        int i = 0;
-        while (rs.next()) {
-            String[] ject;
-            String sub = "";
-            sub += "[" + rs.getString("room") + "] " + this.getCourseName(rs.getString("course_id"));
-            //System.out.println(sub);
-
-            //add subject at table by time
-            ResultSet rc = db.getQuery("SELECT start_time, end_time FROM section WHERE section_id = '" + rs.getString("section_id") + "'");
-            while (rc.next()) {
-                int start = Integer.parseInt(rc.getString("start_time").substring(0, 2)) - 8;
-                int end = Integer.parseInt(rc.getString("end_time").substring(0, 2)) - 9;
-                if (start < 0 || start > 12 || end < 0 || end > 12) {
-                    break;
-                }
-                while (start <= end) {
-                    subject[start] = sub;
-                    start++;
-                }
-                //System.out.println(Arrays.deepToString(subject));
+        try{
+            String q = null;
+            if (user instanceof Student) {
+                q = "SELECT room, course_id, section_id FROM section WHERE section = " + ((Student)user).getSection() + " AND day_of_week = '" + day + "'" ;
             }
+            else if (user instanceof Prof){
+                q = "SELECT room, course_id, section_id FROM section WHERE prof_id = " + ((Prof)user).getProf_id() + " AND day_of_week = '" + day + "'" ;
+            }
+            ResultSet rs = db.getQuery(q);
+            int i = 0;
+            while (rs.next()) {
+                String[] ject;
+                String sub = "";
+                sub += "[" + rs.getString("room") + "] " + this.getCourseName(rs.getString("course_id"));
+                //System.out.println(sub);
+
+                //add subject at table by time
+                ResultSet rc = db.getQuery("SELECT start_time, end_time FROM section WHERE section_id = '" + rs.getString("section_id") + "'");
+                while (rc.next()) {
+                    int start = Integer.parseInt(rc.getString("start_time").substring(0, 2)) - 8;
+                    int end = Integer.parseInt(rc.getString("end_time").substring(0, 2)) - 9;
+                    if (start < 0 || start > 12 || end < 0 || end > 12) {
+                        break;
+                    }
+                    while (start <= end) {
+                        subject[start] = sub;
+                        start++;
+                    }
+                    //System.out.println(Arrays.deepToString(subject));
+                }
+            }
+            //System.out.println(subject);
+            return subject;
+        }catch (SQLException e){
+            return subject;
         }
-        return subject;
     }
 
     //get subject name from another table
