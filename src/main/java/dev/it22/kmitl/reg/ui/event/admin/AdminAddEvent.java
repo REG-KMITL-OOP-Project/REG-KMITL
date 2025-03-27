2e63898d9a8ee6295430c3f8e9c6ce3a82e24912
Hundredz <151929262+NnWAlovemeow@users.noreply.github.com>
2025-03-27 23:01:32 +0700

    change date format

--- src/main/java/dev/it22/kmitl/reg/ui/event/admin/AdminAddEvent.java
+++ src/main/java/dev/it22/kmitl/reg/ui/event/admin/AdminAddEvent.java
@@ -66,9 +66,7 @@ public class AdminAddEvent extends EventPage implements ActionListener {
 
     public void actionPerformed(ActionEvent ev){
         if (ev.getSource() == cancel) {
-
             if (!(eventName.getText().equals("   EVENT NAME") && dateStart.getText().equals("YYYY-MM-DD") && dateEnd.getText().equals("YYYY-MM-DD") && description.getText().equals(("   DESCRIPTION")))){
-
                 dialog = Config.openFrame((int) (frame.getWidth() / 2), (int) (frame.getHeight() / 2));
                 JPanel panelD = new JPanel();
                 JPanel panelC = new JPanel();
