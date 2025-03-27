f8db3177c7210787dcecd498a7343e4fa5d284fc
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-03-28 00:54:21 +0700

    add craete prof and admin

--- src/main/java/dev/it22/kmitl/reg/controller/user/AdminCreateUser.java
+++ src/main/java/dev/it22/kmitl/reg/controller/user/AdminCreateUser.java
@@ -1,13 +1,8 @@
 package dev.it22.kmitl.reg.controller.user;
 
-import dev.it22.kmitl.reg.constant.UserPrefix;
-import dev.it22.kmitl.reg.constant.UserRole;
 import dev.it22.kmitl.reg.controller.auth.Register;
-import dev.it22.kmitl.reg.controller.auth.User;
 import dev.it22.kmitl.reg.utils.Database;
-import dev.it22.kmitl.reg.utils.ErrorModal;
 
-import javax.management.relation.Role;
 
 public class AdminCreateUser {
     private int role;
@@ -55,5 +50,66 @@ public class AdminCreateUser {
         catch (Exception e) {
             System.out.println("Create Student success");
         }
+    }
+
+    public  void createProf(String email,String username,String password,int prefix,String fname,String lname,String phone,String address,String prof_id,String faculty) throws Exception {
+        this.role = 3;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.prefix = prefix;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.address = address;
+        this.prof_id = prof_id;
+        this.faculty = faculty;
+        try{
+            new Register(this.username,this.password).registerWithUsernameAndPassword();
+        }
+        catch (Exception e) {
+            throw new Exception("เกิดข้อผิดพลาดขณะสร้างบัญชี");
+        }
+        // Update Prof data
+        try{
+            Database db = new Database();
+            int status = db.postQuery("UPDATE user SET role = "+this.role+", email = '"+this.email+"', prefix = "+this.prefix+", fname = '"+this.fname+"', lname = '"+this.lname+"', phone = '"+this.phone+"', address = '"+this.address+"', prof_id = '"+this.prof_id+"', faculty = '"+this.faculty+"' WHERE username = '"+this.username+"';");
+            if(status == 0){
+                throw new Exception("เกิดข้อผิดพลาดขณะอัพเดทข้อมูล");
+            }
+            System.out.println("Create Prof success");
+        }
+        catch (Exception e) {
+            System.out.println("Create Prof success");
+        }
+    }
+
+    public void createAdmin(String email,String username,String password,int prefix,String fname,String lname) throws Exception {
+        this.role = 1;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.prefix = prefix;
+        this.fname = fname;
+        this.lname = lname;
+        try{
+            new Register(this.username,this.password).registerWithUsernameAndPassword();
+        }
+        catch (Exception e) {
+            throw new Exception("เกิดข้อผิดพลาดขณะสร้างบัญชี");
+        }
+        // Update Admin data
+        try{
+            Database db = new Database();
+            int status = db.postQuery("UPDATE user SET role = "+this.role+", email = '"+this.email+"', prefix = "+this.prefix+", fname = '"+this.fname+"', lname = '"+this.lname+"' WHERE username = '"+this.username+"';");
+            if(status == 0){
+                throw new Exception("เกิดข้อผิดพลาดขณะอัพเดทข้อมูล");
+            }
+            System.out.println("Create Admin success");
+        }
+        catch (Exception e) {
+            System.out.println("Create Admin success");
+        }
+
     }
 }
