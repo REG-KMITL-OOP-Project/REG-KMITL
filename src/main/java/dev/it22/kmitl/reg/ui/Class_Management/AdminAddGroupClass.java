c263ab8b77849297c5d057b9c2174d50129de136
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2025-04-01 17:22:21 +0700

    AdminAddGroup connect to database

--- src/main/java/dev/it22/kmitl/reg/ui/Class_Management/AdminAddGroupClass.java
+++ src/main/java/dev/it22/kmitl/reg/ui/Class_Management/AdminAddGroupClass.java
@@ -5,15 +5,18 @@ import dev.it22.kmitl.reg.utils.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AdminAddGroupClass implements FocusListener , ActionListener {
     private JFrame frame ;
-    private JPanel panelBig,panelRek1,panelRek2,panelRek3,panelRek5,panelHead,panelTime,panelTimeExamMid,panelTimeExamFinal,groupPan,numPan,typePan,towPan,roomPan;
-    private JLabel numGroupLabel,numStuLabel,typeLabel,addGroup,date,timeExam,timeExamFinal,towLabel,roomLabel;
+    private JPanel panelBig,panelRek1,panelRek2,panelRek3,panelRek5,panelHead,panelTime,panelTimeExamMid, panelDateExamMid,panelTimeExamFinal,groupPan,numPan,typePan,towPan,roomPan,panelTimeExamMidEnd,panelDateExamFinal,panelTimeExamFinalEnd;
+    private JLabel numGroupLabel,numStuLabel,typeLabel,addGroup,dateExam,timeExam,dateExamFinal,timeExamFinal, timeExamFinalEnd,timeExamEnd,towLabel,roomLabel;
     private RoundedTextField numGroup,numStu,time,mid,fi,room;
-    private JComboBox type,tow;
+    private JComboBox tow;
+    private CourseNameCombo type;
     private Font innerFont, regularFont;
+    private TimeComboBox timeMid, timeMidEnd, timeFinal, timeFinalEnd;
     private RoundedButton cancel,save;
     private JPanel panelSave = new JPanel() , panelCan = new JPanel();
     protected boolean showGroup, showStu, showTime, showMid,showFi,showRoom;
@@ -34,13 +37,26 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
         typePan = new JPanel();
         towPan = new JPanel();
         roomPan = new JPanel();
+        panelTimeExamMidEnd = new JPanel();
+        panelDateExamMid = new JPanel();
+        panelDateExamFinal = new JPanel();
+        panelTimeExamFinal = new JPanel();
+        panelTimeExamFinalEnd = new JPanel();
+        timeMid =  new TimeComboBox();
+        timeMidEnd = new TimeComboBox();
+        timeFinal = new TimeComboBox();
+        timeFinalEnd = new TimeComboBox();
         numGroupLabel = new JLabel("จำนวนกลุ่มเรียน");
         numStuLabel = new JLabel("จำนวนนักศึกษาที่รับ");
         typeLabel = new JLabel("วิชา");
         addGroup = new JLabel("              เพิ่มกลุ่มเรียน");
         //date = new JLabel("วัน-เวลาเรียน");
-        timeExam = new JLabel("วัน-เวลาสอบกลางภาค");
-        timeExamFinal = new JLabel("วัน-เวลาสอบปลายภาค");
+        dateExam = new JLabel("วันสอบกลางภาค");
+        timeExam = new JLabel("เวลาเริ่มสอบกลางภาค");
+        timeExamEnd = new JLabel("หมดเวลาสอบกลางภาค");
+        dateExamFinal = new JLabel("วันสอบปลายภาค");
+        timeExamFinal = new JLabel("เวลาเริ่มสอบปลายภาค");
+        timeExamFinalEnd = new JLabel("หมดเวลาสอบปลายภาค");
         //towLabel = new JLabel("อาคารเรียน");
         //roomLabel = new JLabel("ห้องเรียน");
         numGroup = new RoundedTextField(22);
@@ -51,7 +67,7 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
         numStu = new RoundedTextField(22);
         cancel = new RoundedButton("CANCEL" ,22);
         save = new RoundedButton("SAVE" ,22);
-        type = new JComboBox();
+        type = new CourseNameCombo();
         //tow = new JComboBox();
         regularFont = Config.NORMAL_REGULAR;
         innerFont = regularFont.deriveFont(15f);
@@ -70,6 +86,10 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
         groupPan.setBackground(null);
         numPan.setBackground(null);
         typePan.setBackground(null);
+        panelDateExamMid.setBackground(null);
+        panelTimeExamMidEnd.setBackground(null);
+        panelDateExamFinal.setBackground(null);
+        panelTimeExamFinalEnd.setBackground(null);
         //towPan.setBackground(null);
         //roomPan.setBackground(null);
 
@@ -117,9 +137,6 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
         numGroup.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
 
         panelRek1.add(typePan);
-        type.addItem("OOP");
-        type.addItem("DSA");
-        type.addItem("BFIT");
         type.setRenderer(new CustomCombobox());
         type.setMaximumRowCount(3);
         type.setFont(Config.NORMAL_REGULAR);
@@ -144,19 +161,64 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
             date.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
         */
 
+        panelDateExamMid.setLayout(new BorderLayout());
+        panelDateExamMid.add(dateExam,BorderLayout.NORTH);
+        panelDateExamMid.add(mid);
+        dateExam.setForeground(Config.primaryColor_base);
+        dateExam.setFont(Config.HEADER_SEMIBOLD[2]);
+        dateExam.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+
         panelTimeExamMid.setLayout(new BorderLayout());
         panelTimeExamMid.add(timeExam,BorderLayout.NORTH);
-        panelTimeExamMid.add(mid);
+        panelTimeExamMid.add(timeMid);
+            timeMid.setMaximumRowCount(3);
+            timeMid.setFont(Config.NORMAL_REGULAR);
+            timeMid.setFont(innerFont);
+            timeMid.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
             timeExam.setForeground(Config.primaryColor_base);
             timeExam.setFont(Config.HEADER_SEMIBOLD[2]);
             timeExam.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
 
+        panelTimeExamMidEnd.setLayout(new BorderLayout());
+        panelTimeExamMidEnd.add(timeExamEnd,BorderLayout.NORTH);
+        panelTimeExamMidEnd.add(timeMidEnd);
+        timeMidEnd.setMaximumRowCount(3);
+        timeMidEnd.setFont(Config.NORMAL_REGULAR);
+        timeMidEnd.setFont(innerFont);
+        timeMidEnd.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+        timeExamEnd.setForeground(Config.primaryColor_base);
+        timeExamEnd.setFont(Config.HEADER_SEMIBOLD[2]);
+        timeExamEnd.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+
+        panelDateExamFinal.setLayout(new BorderLayout());
+        panelDateExamFinal.add(dateExamFinal,BorderLayout.NORTH);
+        panelDateExamFinal.add(fi);
+        //panelDateExamFinal.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));;
+            dateExamFinal.setForeground(Config.primaryColor_base);
+            dateExamFinal.setFont(Config.HEADER_SEMIBOLD[2]);
+            dateExamFinal.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+
         panelTimeExamFinal.setLayout(new BorderLayout());
         panelTimeExamFinal.add(timeExamFinal,BorderLayout.NORTH);
-        panelTimeExamFinal.add(fi);
-            timeExamFinal.setForeground(Config.primaryColor_base);
-            timeExamFinal.setFont(Config.HEADER_SEMIBOLD[2]);
-            timeExamFinal.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+        panelTimeExamFinal.add(timeFinal);
+        timeFinal.setMaximumRowCount(3);
+        timeFinal.setFont(Config.NORMAL_REGULAR);
+        timeFinal.setFont(innerFont);
+        timeFinal.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+        timeExamFinal.setForeground(Config.primaryColor_base);
+        timeExamFinal.setFont(Config.HEADER_SEMIBOLD[2]);
+        timeExamFinal.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+
+        panelTimeExamFinalEnd.setLayout(new BorderLayout());
+        panelTimeExamFinalEnd.add(timeExamFinalEnd,BorderLayout.NORTH);
+        panelTimeExamFinalEnd.add(timeFinalEnd);
+        timeFinalEnd.setMaximumRowCount(3);
+        timeFinalEnd.setFont(Config.NORMAL_REGULAR);
+        timeFinalEnd.setFont(innerFont);
+        timeFinalEnd.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
+        timeExamFinalEnd.setForeground(Config.primaryColor_base);
+        timeExamFinalEnd.setFont(Config.HEADER_SEMIBOLD[2]);
+        timeExamFinalEnd.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
 
         /*panelRek2.add(panelTime);
         showTime = true;
@@ -166,58 +228,31 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
         time.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
         time.addFocusListener(this);
         */
+        panelRek2.add(panelDateExamMid);
         panelRek2.add(panelTimeExamMid);
+        panelRek2.add(panelTimeExamMidEnd);
         showMid = true;
-        mid.setText("   จ. 9:00 - 12:00");
+        mid.setText("   2025-01-30");
         mid.setFont(innerFont);
         mid.setForeground(Color.GRAY);
-        mid.setPreferredSize(new Dimension((int)(frame.getWidth() / 2.65),(frame.getHeight() / 4) - 120));
+        mid.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
         mid.addFocusListener(this);
 
-        panelRek2.add(panelTimeExamFinal);
         showFi = true;
-        fi.setText("   จ. 9:00 - 12:00");
+        fi.setText("   2025-01-30");
         fi.setFont(innerFont);
         fi.setForeground(Color.GRAY);
-        fi.setPreferredSize(new Dimension((int)(frame.getWidth() / 2.65),(frame.getHeight() / 4) - 120));
+        fi.setPreferredSize(new Dimension((int)(frame.getWidth() / 4),(frame.getHeight() / 4) - 120));
         fi.addFocusListener(this);
 
         panelBig.add(panelRek2);
 
-        /*panelRek3.setPreferredSize(new Dimension((int)(frame.getWidth()),(int)(frame.getHeight() / 3.5) - 120));
-        towPan.setLayout(new BorderLayout());
-        towPan.add(towLabel,BorderLayout.NORTH);
-        towPan.add(tow);
-        towLabel.setForeground(Config.primaryColor_base);
-        towLabel.setFont(Config.HEADER_SEMIBOLD[2]);
-        towLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+        panelRek3.setPreferredSize(new Dimension((int)(frame.getWidth()),(int)(frame.getHeight() / 3.5) - 120));
+        panelRek3.add(panelDateExamFinal);
+        panelRek3.add(panelTimeExamFinal);
+        panelRek3.add(panelTimeExamFinalEnd);
 
-        roomPan.setLayout(new BorderLayout());
-        roomPan.add(roomLabel,BorderLayout.NORTH);
-        roomPan.add(room);
-        roomLabel.setForeground(Config.primaryColor_base);
-        roomLabel.setFont(Config.HEADER_SEMIBOLD[2]);
-        roomLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
-
-        panelRek3.add(towPan);
-        tow.addItem("IT");
-        tow.addItem("อาคารพระเทพ");
-        tow.addItem("คณะบริหาร");
-        tow.setRenderer(new CustomCombobox());
-        tow.setMaximumRowCount(3);
-        tow.setFont(Config.NORMAL_REGULAR);
-        tow.setFont(innerFont);
-        tow.setPreferredSize(new Dimension((int)(frame.getWidth() / 2.5),(frame.getHeight() / 4) - 120));
-
-        panelRek3.add(roomPan);
-        showRoom = true;
-        room.setText("   M22");
-        room.setFont(innerFont);
-        room.setForeground(Color.GRAY);
-        room.setPreferredSize(new Dimension((int)(frame.getWidth() / 2.8),(frame.getHeight() / 4) - 120));
-        room.addFocusListener(this);
-
-        panelBig.add(panelRek3);*/
+        panelBig.add(panelRek3);
 
         panelRek5.add(panelCan);
         panelCan.setLayout( new FlowLayout(FlowLayout.LEFT,93,0));
@@ -250,11 +285,6 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
         new AdminAddGroupClass(Config.createAndShowGUI());
     }
     public void focusGained(FocusEvent fg){
-        try{
-            int db = new Database().postQuery("INSERT INTO section");
-        } catch (Exception e) {
-
-        }
         if (fg.getSource().equals(numGroup) && showGroup){
             numGroup.setText("");
             numGroup.setForeground(Color.BLACK);
@@ -302,11 +332,11 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
 
         }*/if (mid.getText().isEmpty()) {
             showMid = true;
-            mid.setText("   จ. 9:00 - 12:00");
+            mid.setText("   2025-01-30");
             mid.setForeground(Color.GRAY);
         }if (fi.getText().isEmpty()) {
             showFi = true;
-            fi.setText("   จ. 9:00 - 12:00");
+            fi.setText("   2025-01-30");
             fi.setForeground(Color.GRAY);
         }/*if (room.getText().isEmpty()) {
             showRoom = true;
@@ -319,7 +349,7 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
 
     public void actionPerformed(ActionEvent ev){
         if (ev.getSource() == cancel) {
-            if (!(numGroup.getText().equals("   3") && numStu.getText().equals("    200") && /*time.getText().equals("   จ. 8:00 - 20:00") && */mid.getText().equals(("   จ. 9:00 - 12:00")) && fi.getText().equals(("   จ. 9:00 - 12:00")) /*&& room.getText().equals(("   M22"))*/ )){
+            if (!(numGroup.getText().equals("   3") && numStu.getText().equals("    200") && /*time.getText().equals("   จ. 8:00 - 20:00") && */mid.getText().equals(("   2025-01-30")) && fi.getText().equals(("   2025-01-30")) /*&& room.getText().equals(("   M22"))*/ )){
                 dialog = Config.openFrame((int) (frame.getWidth() / 2), (int) (frame.getHeight() / 2));
                 JPanel panelD = new JPanel();
                 JPanel panelC = new JPanel();
@@ -378,8 +408,43 @@ public class AdminAddGroupClass implements FocusListener , ActionListener {
             dialog.setVisible(false);
         }else if (ev.getSource().equals(yes) || ev.getSource().equals(save)){
             if(ev.getSource().equals(save)){
-                if (numGroup.getText().equals("   3") || numStu.getText().equals("    200") ||/* time.getText().equals("   จ. 8:00 - 20:00") || */mid.getText().equals(("   จ. 9:00 - 12:00")) || fi.getText().equals(("   จ. 9:00 - 12:00")) /*&& room.getText().equals(("   M22")) */) {
-                    new ErrorModal(frame, "กรุณากรอกข้อมูลให้ครบถ้วน");
+                try {
+                    Double.parseDouble(numGroup.getText());
+                    Double.parseDouble(numStu.getText());
+                    if (numGroup.getText().equals("   3") || numStu.getText().equals("    200") ||/* time.getText().equals("   จ. 8:00 - 20:00") || */mid.getText().equals(("   2025-01-30")) || fi.getText().equals(("   2025-01-30")) || timeMid.getSelectedIndex() > timeMidEnd.getSelectedIndex() || timeFinal.getSelectedIndex() > timeFinalEnd.getSelectedIndex()) {
+                        if (timeMid.getSelectedIndex() > timeMidEnd.getSelectedIndex() || timeFinal.getSelectedIndex() > timeFinalEnd.getSelectedIndex()) {
+                            new ErrorModal(frame, "กรุณาเลือกเวลาสอบให้ถูกต้อง");
+                            return;
+                        } else {
+                            new ErrorModal(frame, "กรุณากรอกข้อมูลให้ครบถ้วน");
+                            return;
+                        }
+                    } else {
+                        try {
+                            String[] abc = {"A", "B", "C", "D", "E", "F", "G", "H" };
+                            int num = Integer.parseInt(numStu.getText());
+                            for (int i = 1; i <= Integer.valueOf(numGroup.getText()); i++) {
+                                ResultSet rs = new Database().getQuery("SELECT * FROM section WHERE section_id = '" + type.getCourseCode() + abc[i - 1] + "'");
+                                if (rs.next()) {
+                                    new ErrorModal(frame, "วิชานี้มีข้อมูลกลุ่มเรียนอยู่แล้ว โปรดเลือกวิชาอื่น");
+                                    return;
+                                } else {
+                                    if (i == Integer.valueOf(numGroup.getText())) {
+                                        int db = new Database().postQuery("INSERT INTO section (section_id , course_id, prof_id, day_of_week, start_time, end_time, building, room, max_std, section, prof_name) VALUES ('" + type.getCourseCode() + abc[i - 1] + "', '" + type.getCourseCode() + "', '2555070002', 'MON','','','','','" + num + "', '" + i + "', '');");
+                                    } else {
+                                        num -= (int) (Double.valueOf(numStu.getText()) / Double.valueOf(numGroup.getText()));
+                                        int db = new Database().postQuery("INSERT INTO section (section_id , course_id, prof_id, day_of_week, start_time, end_time, building, room, max_std, section, prof_name) VALUES ('" + type.getCourseCode() + abc[i - 1] + "', '" + type.getCourseCode() + "', '2555070002', 'MON','','','','','" + (int) (Double.valueOf(numStu.getText()) / Double.valueOf(numGroup.getText())) + "', '" + i + "', '');");
+                                    }
+                                    int db = new Database().postQuery("INSERT INTO exam (section_id , course_id, midterm_date, midterm_start_time, midterm_end_time, final_date, final_start_time, final_end_time, subject, type, room, prof_name, section) VALUES ('" + type.getCourseCode() + abc[i - 1] + "', '" + type.getCourseCode() + "', '" + mid.getText() + "', '" + timeMid.getSelectedItem() + "','" + timeMidEnd.getSelectedItem() + "','" + fi.getText() + "','" + timeFinal.getSelectedItem() + "','" + timeFinalEnd.getSelectedItem() + "','" + type.getCourseName() + "', 'ทฤษฎี', '', '', '" + i + "');");
+                                }
+                            }
+                            int db = new Database().postQuery("UPDATE exam SET midterm_date = '" + mid.getText() + "', midterm_start_time = '" + timeMid.getSelectedItem() + "', midterm_end_time = '" + timeMidEnd.getSelectedItem() + "', final_date ='" + fi.getText() + "', final_start_time = '" + timeFinal.getSelectedItem() + "', final_end_time = '" + timeFinalEnd.getSelectedItem() + "' WHERE course_id = '" + type.getCourseCode() + "';");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }catch (Exception e){
+                    new ErrorModal(frame, "กรุณากรอกกลุ่มเรียนหรือจำนวนนักศึกษาให้ถูกต้อง");
                     return;
                 }
             }
