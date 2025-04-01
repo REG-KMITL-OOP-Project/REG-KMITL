2eff490712cc5dd477c2cd29ec4f7f129aa59fba
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2025-04-01 11:15:34 +0700

    Change Time to be a 30 minute

--- src/main/java/dev/it22/kmitl/reg/utils/TimeComboBox.java
+++ src/main/java/dev/it22/kmitl/reg/utils/TimeComboBox.java
@@ -8,7 +8,7 @@ public class TimeComboBox extends JComboBox{
     public TimeComboBox() {
         super();
         DateTimeFormatter dft = DateTimeFormatter.ofPattern("HH:mm:ss");
-        for(LocalTime time = LocalTime.of(6,0); time.isBefore(LocalTime.of(22, 00)); time = time.plusHours(1)) {
+        for(LocalTime time = LocalTime.of(6,0); time.isBefore(LocalTime.of(22, 00)); time = time.plusMinutes(30)) {
             this.addItem(time.format(dft));
             this.setRenderer(new CustomCombobox());
         }
